6f5a7b422bf879b17c1ba0ef93e5b990ad62b458
dixitudit <58696687+dixitudit@users.noreply.github.com>
2024-08-21 23:12:35 +0530

    debug

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ const config = {
     API_URL: "http://localhost:5000",
   },
   production: {
-    API_URL: "https://chat-app-2gnu.onrender.com/",
+    API_URL: "https://chat-app-2gnu.onrender.com",
   },
 };
 
